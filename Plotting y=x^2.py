e45ede565dea55f0dcbd61c833caacebf432ff80
Aaron Liddell <aaronliddell@gmail.com>
2025-09-17 20:50:09 +0100

    push for the graph

---
+++ Plotting y=x^2.py
@@ -0,0 +1,13 @@
+import math
+import matplotlib.pyplot as plt
+
+yData = []
+xData = []
+
+for i in range(0,11):
+    y = i**2
+    yData.append(y)
+    xData.append(i)
+
+plt.plot(xData, yData)
+plt.show()
