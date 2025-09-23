605cf43656ffbd2757d4f79d744233182528c628
Aaron Liddell <aaronliddell@gmail.com>
2025-09-23 14:47:33 +0100

    Hypo calc complete

--- Hypotenuse Finder/Main.py
+++ Hypotenuse Finder/Main.py
@@ -1,1 +1,12 @@
-"""helloooo"""
+import math as m
+
+def getData():
+    x = int(input("Input side one of triangle: "))
+    y = int(input("Input side two of triangle: "))
+    hypo(x,y)
+
+def hypo(x,y):
+    hypo = m.sqrt(x**2 + y**2)
+    print(hypo)
+
+getData()
